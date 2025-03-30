2533478d8c8c91c8c644d3786d4656e5b7c2c33b
krapna <krapna02@gmail.com>
2025-03-30 19:52:36 +0200

    Úprava Script1.js - přesunutí tlačítka Odstranit na pravou stranu

--- Server.js
+++ Server.js
@@ -105,18 +105,17 @@ app.post('/uploadToDropbox', async (req, res) => {
     // Očekáváme JSON s base64Data (bez prefixu) a fileName
     const { base64Data, fileName } = req.body;
 
-    // Přístupový token se načítá z environmentálních proměnných
+    // PŘÍMO VLOŽENÝ Access Token
+    // (POZOR: není to doporučené řešení pro produkční nasazení)
     const dbx = new Dropbox({
-      accessToken: process.env.DROPBOX_ACCESS_TOKEN,
+      accessToken: 'sl.u.AFq4a8kog5h5Cogdpmgp8rKBw7yn_agmEyf-DG2Be6B-YYhDyt-qC2KwLee5awSsEc1EwfrAanvPnU1TrzIsjGA6FXiH1rAxkjqATCZNZJVeUbbcIzJKV2lOKhx8D1FujCJ98QMVRFHNEF7IerFXG9I3a6r8YpvAmn3GfWkjYIMjXIB6SbBdrqiQ-OWwBjj6aezG0WjPtWEDikxgeGKBBZLR9-qsTH0fNrkoC0N6H-19auovHIEgkiWg2D3mrQYBIARpmTroYj1s-kijMVmoX_T3l1B7oEsr9D7KQqSzGJ1aos5C0qxhFQ_ru0pFKCLujHKSr9jQTY9HEHZPyobiNtqDGIKGX2GVahImU8QADYw59BUBGj8k60Nm9j6g40ufKD2RCuwjo7R3QmLJNQ3riphzaabZHJ5h4nSneTldGH6oQs7oUTru2NHbBr2ezsgxVEFukwaAJ1WLHjaML5bZnl4I3VUNV67JknkUxDAw56x4fG5OyDkQ9H0UzReWk50N9cehGZkpcVZnFxKR-l9MiLIVxsXPdWvU0q54p76UdfbL0p8Bm9Jm6tp65vu2DNSy28qMaPjpFCDyZcqPI0begtfCv1Xw0Mbe7PaEvHw0mpBnmRVRckDtxvzI7QceyG0fdtsyrTCyqySuepx_vs_bbKswiDq9LAbRCcYZv_hZ-ddg40q0Z0VEa19G_IeIiKXn0k_MxwToLBERy_ERq9ZcgD14NcZlAjc-Ev18oga36xJoIVLrg-BoGhotT9j_ba3--C_B2XwRs2ewTwYCGeJpz7ezC1tRyuvkMV9GteFauTw81h4YzPZb5UE2xLj61r5qQtCPcG-8_t3sPwd9WNro6XHiWBgavGrtIbcqziVSuxmt1Po3gCC8jnpBwwNyKKV7r1BdIRfQW_EWrFhpJ1TS8KWDaNmcEEfv0nwshiMZ-lo40lWD1O8kI8BTN0x4s7bEUUkUyNGIQQG2-F2ADHjVf9-cNkD8UWhE4iMZcfErt4ZgeYVIBnCwLKuG3OyS7zq9BUDu7KUuuEwGSzlWrKcE0QaVDlbI-D0X4wL8iEqSD6GYO2onxYAPjQEi4INAItU_73_BqSIvrdN-dRDjapT-0pWVWNGAlj-uSJ2u0qBwOQVqoGJRlGdJEpL9VZYWhI8pQbGkECoELl71Af9SwTyj1tc_3Qk5JDGB8XvB1HaXs2iRyzAkCxapvenG7uSarxAAqaoHPiIO-EE5YdGUDpRApxU-QB1rf_fO9fygpcLzMf1CWzCWW5FlwLgEAjdUS9zW-vlldmCwN1F-HUdWK8Ut-QXNRkosWKLDSkR6W0VOx0cA9VdJl4__o9mD2iGutKJ35EFeLwfgrTLGh9xP1yQWGlfn46ndcsFdTsj1yMQJ5huXmrN6o9-ztQJhninYXyi7C6s83C2t5yX80G0stKjEKbNAaZQOUL1aiSoglv4pKbnQ2w',
       fetch: fetch
     });
 
     // Převod base64 na binární buffer
     const fileBuffer = Buffer.from(base64Data, 'base64');
 
-    // Nastav cestu, kam se soubor uloží v Dropboxu.
-    // Pokud chceš, aby se nahrával přímo do kořene, použij '/' + fileName
-    // Pokud chceš do složky, např. '/app/', uprav dle potřeby.
+    // Cesta, kam se soubor uloží v Dropboxu (zde do kořene "/")
     const dropboxPath = '/' + fileName;
 
     // Nahraj soubor do Dropboxu
