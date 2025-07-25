6e1750a7b70e794ed4e0033f6b8423dc54283cf1
krapna <krapna02@gmail.com>
2025-07-25 12:38:57 +0200

    Úprava Script1.js - přesunutí tlačítka Odstranit na pravou stranu

--- public/onlineserv.js
+++ public/onlineserv.js
@@ -1,9 +1,11 @@
+// onlineserv.js
 document.addEventListener('DOMContentLoaded', () => {
     // Připojení ke serveru přes socket.io a uložení do window.socket pro další použití
     const socket = io();
     window.socket = socket;
 
-    socket.on('connect', () => {
+    // Požádáme o data pouze jednou, hned po prvním připojení
+    socket.once('connect', () => {
         console.log('Připojeno k serveru pro synchronizaci pomocí socket.io');
         socket.emit('requestDocuments');
     });
